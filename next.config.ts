c236dfaa6d666840ad80662e310297824c6fae8c
Mubeen Shafiq <muhammadmubeen9182@gmail.com>
2025-04-20 16:31:25 +0500

    setup boiler plate files, added font, added redirect, initial clean

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/app/chat",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
