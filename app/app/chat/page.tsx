ae2a50e38b3e0e1479c4e87f597ea49ad8706106
Mubeen Shafiq <muhammadmubeen9182@gmail.com>
2025-04-20 17:42:06 +0500

    setup antd and theme toggle

--- app/app/chat/page.tsx
+++ app/app/chat/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+import { Button } from "antd";
+import { useTheme } from "next-themes";
+
 function ChatPage() {
-  return <div>ChatPage</div>;
+  const { setTheme } = useTheme();
+  return (
+    <div>
+      <Button type="primary" onClick={() => setTheme("light")}>
+        Toggle Theme
+      </Button>
+    </div>
+  );
 }
 
 export default ChatPage;
