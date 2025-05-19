d1ba8b111493859a1f6de90e91c193cb8dd2563d
Mubeen Shafiq <muhammadmubeen9182@gmail.com>
2025-05-19 11:12:54 +0500

    working sidebar design

--- app/components/app/chats/sidebar.tsx
+++ app/components/app/chats/sidebar.tsx
@@ -4,8 +4,13 @@ import Sider from "antd/es/layout/Sider";
 import React, { CSSProperties } from "react";
 import AppLogo from "../../common/app-logo";
 import { SearchBox } from "../../common/search-box";
-import { Avatar, List, Space } from "antd";
-import Link from "next/link";
+import { Menu, Space } from "antd";
+import {
+  CheckOutlined,
+  FieldTimeOutlined,
+  ReadOutlined,
+} from "@ant-design/icons";
+import { ItemType, MenuItemType } from "antd/es/menu/interface";
 
 const siderStyles: CSSProperties = {
   position: "sticky",
@@ -16,56 +21,85 @@ const siderStyles: CSSProperties = {
   padding: "1rem",
 };
 
-const data = [
+const data: ItemType<MenuItemType>[] = [
   {
-    _id: "object_id_1",
-    avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=0",
-    name: "Contact 1",
-    lastMessage: { content: "Hi There From Old Friend!" },
-  },
-  {
-    _id: "object_id_2",
-    avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
-    name: "Contact 1",
-    lastMessage: { content: "Hi There From Old Friend!" },
-  },
-  {
-    _id: "object_id_3",
-    avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=2",
-    name: "Contact 1",
-    lastMessage: { content: "Hi There From Old Friend!" },
-  },
-  {
-    _id: "object_id_4",
-    avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=3",
-    name: "Contact 1",
-    lastMessage: { content: "Hi There From Old Friend!" },
+    id: "favorites",
+    label: "Favorites",
+    type: "group",
+    children: [
+      {
+        id: "object_id_1",
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=0",
+        name: "Contact 1",
+        lastMessage: {
+          content: "Hi There From Old Friend!",
+          readAt: new Date("12-12-2024"),
+          sentAt: new Date("11-12-2024"),
+        },
+      },
+      {
+        id: "object_id_2",
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        name: "Contact 1",
+        lastMessage: {
+          content: "Hi There From Old Friend!",
+          readAt: null,
+          sentAt: new Date("10-12-2024"),
+        },
+      },
+      {
+        id: "object_id_3",
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=2",
+        name: "Contact 1",
+        lastMessage: {
+          content: "Hi There From Old Friend!",
+          readAt: new Date("12-12-2024"),
+          sentAt: new Date("11-12-2024"),
+        },
+      },
+      {
+        id: "object_id_4",
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=3",
+        name: "Contact 1",
+        lastMessage: {
+          content: "Hi There From Old Friend!",
+          readAt: null,
+          sentAt: null,
+        },
+      },
+    ],
   },
 ];
 
+const MessageStatusIcons = {
+  sent: {
+    icon: CheckOutlined,
+    style: { color: "gray" },
+  },
+  read: {
+    icon: ReadOutlined,
+  },
+  enqueue: {
+    icon: FieldTimeOutlined,
+    style: { color: "gray" },
+  },
+};
+
+const getMessageStatus = (readAt: Date | null, sentAt: Date | null) => {
+  if (readAt) return "read";
+  if (sentAt) return "sent";
+  return "enqueue";
+};
+
 export const RenderContactsList = () => (
-  <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item) => (
-      <List.Item key={item._id}>
-        <Link href={`/app/chat/${item._id}`} style={{ width: "100%" }}>
-          <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<p>{item.name}</p>}
-            description={item.lastMessage.content}
-          />
-        </Link>
-      </List.Item>
-    )}
-  />
+  <Menu mode="inline" theme="light" items={data} />
 );
 
 export const ChatsSidebar = () => {
   return (
     <Sider
       style={siderStyles}
-      width="280px"
+      width="320px"
       breakpoint="lg"
       collapsedWidth="0"
       theme="light"
